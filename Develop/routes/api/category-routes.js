ba5ee6efc972f0d3416dd38f094780e61075a6e6
Kajal Patel <kajalpatel20@gmail.com>
2021-03-23 20:03:45 -0400

    start database

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,10 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll()
+  .then((categories) => res.json(categories))
+  .catch(err => res.status(500).json(err))
+  
 });
 
 router.get('/:id', (req, res) => {
